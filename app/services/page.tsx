ec0459334297ab270cab9fc7726a3c303e3f6b94
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-04 15:20:50 +0300

    commit

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from "react";
-import CustomCheckBox from "../_components/CustomCheckBox/CustomCheckBox";
+import CustomCheckBoxText from "../_components/CustomCheckBox/CustomCheckBoxText";
 
 export default function page() {
   return (
     <div>
       <h1>ddddd</h1>
-
+      <CustomCheckBoxText btnSize="sm" label="hiiiiiiii" inputType="checkbox" />
+      <CustomCheckBoxText btnSize="md" label="hiiiiiiii" inputType="checkbox" />
+      <CustomCheckBoxText btnSize="lg" label="hiiiiiiii" inputType="checkbox" />
+      <CustomCheckBoxText btnSize="xl" label="hiiiiiiii" inputType="checkbox" />
        
   
 
