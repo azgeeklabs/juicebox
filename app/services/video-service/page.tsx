a6cb7d95f3a0427cd7d9577166abf7b26cfc6242
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-06-04 17:13:44 +0300

    edit start in footage selection

--- app/services/video-service/page.tsx
+++ app/services/video-service/page.tsx
@@ -6,43 +6,43 @@ import CustomCheckBoxText from "@/app/_components/customCheckBox/CustomCheckBoxT
 import Link from "next/link";
 
 export default function page() {
-  useEffect(() => {
-    let items = document.getElementsByClassName("item") as any;
-    let next = document.getElementById("next");
-    let prev = document.getElementById("prev");
+  // useEffect(() => {
+  //   let items = document.getElementsByClassName("item") as any;
+  //   let next = document.getElementById("next");
+  //   let prev = document.getElementById("prev");
 
-    let active = 3;
-    function loadShow() {
-      let stt = 0;
-      items[active].style.transform = `none`;
-      items[active].style.zIndex = 1;
-      items[active].style.filter = "none";
-      items[active].style.opacity = 1;
-      for (var i = active + 1; i < items.length; i++) {
-        stt++;
-        items[i].style.transform = `translateX(${120 * stt}px) scale(${
-          1 - 0.2 * stt
-        }) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = "blur(5px)";
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-      }
-      stt = 0;
-      for (var i = active - 1; i >= 0; i--) {
-        stt++;
-        items[i].style.transform = `translateX(${-120 * stt}px) scale(${
-          1 - 0.2 * stt
-        }) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = "blur(5px)";
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-      }
-    }
+  //   let active = 3;
+  //   function loadShow() {
+  //     let stt = 0;
+  //     items[active].style.transform = `none`;
+  //     items[active].style.zIndex = 1;
+  //     items[active].style.filter = "none";
+  //     items[active].style.opacity = 1;
+  //     for (var i = active + 1; i < items.length; i++) {
+  //       stt++;
+  //       items[i].style.transform = `translateX(${120 * stt}px) scale(${
+  //         1 - 0.2 * stt
+  //       }) perspective(16px) rotateY(-1deg)`;
+  //       items[i].style.zIndex = -stt;
+  //       items[i].style.filter = "blur(5px)";
+  //       items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  //     }
+  //     stt = 0;
+  //     for (var i = active - 1; i >= 0; i--) {
+  //       stt++;
+  //       items[i].style.transform = `translateX(${-120 * stt}px) scale(${
+  //         1 - 0.2 * stt
+  //       }) perspective(16px) rotateY(1deg)`;
+  //       items[i].style.zIndex = -stt;
+  //       items[i].style.filter = "blur(5px)";
+  //       items[i].style.opacity = stt > 2 ? 0 : 0.6;
+  //     }
+  //   }
 
-    console.log(items);
+  //   console.log(items);
 
-    // loadShow();
-  }, []);
+  //   // loadShow();
+  // }, []);
 
   return (
     <div className=" flex flex-col justify-between h-full">
@@ -60,11 +60,11 @@ export default function page() {
         </div>
         <div className=" flex items-center justify-between pl-[4.021vw] pr-[1vw]">
           <div className={`${styles.btns} flex flex-col gap-[1.041vw]`}>
-            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Explainer</CustomCheckBoxText>
-            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Educational</CustomCheckBoxText>
-            <CustomCheckBoxText btnSize="xl" inputType="checkbox">YouTube</CustomCheckBoxText>
-            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Social Media Ad</CustomCheckBoxText>
-            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Short Film</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="radio" name="type">Explainer</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="radio" name="type">Educational</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="radio" name="type">YouTube</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="radio" name="type">Social Media Ad</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="radio" name="type">Short Film</CustomCheckBoxText>
           </div>
           <div>
             <div className={`${styles.slider} slider`}>
