9fc26417585d8237cddec3fa800bce65e92b79e0
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-06-04 14:02:39 +0300

    start video service

--- app/services/video-service/page.tsx
+++ app/services/video-service/page.tsx
@@ -1,8 +1,20 @@
 "use client";
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className=" bg-red-500 w-full">
+      <div className=" pt-[1.5vw] pl-[4.264vw] mb-[2.933vw]">
+        <h2 className=" mb-[1.041vw]"> What type of video would you like to create?</h2>
+        <h4>
+        Our talented video editors possess a wide range of skills that can create <hr className="border-0"/> different genres of videos depending on your needs!
+        </h4>
+      </div>
+      <div className=" flex justify-between items-center pl-[4.021vw] pr-[1vw]">
+          <div className=" flex flex-col gap-[1.041vw]">
+            
+          </div>
+      </div>
+    </div>
+  );
 }
