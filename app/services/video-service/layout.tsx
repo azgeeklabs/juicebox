3d2d0605b7a1aee5513367c4f6c19f8db8edabf6
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-06-04 16:38:48 +0300

    finish first video service

--- app/services/video-service/layout.tsx
+++ app/services/video-service/layout.tsx
@@ -1,22 +1,16 @@
+import StepProgress from "@/app/_components/stepProgress/StepProgress";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
+    
   return (
-    <div className=" px-[1vw] py-[0.85vw] ">
-      <div className=" mb-[2.46vw] rounded-[72px] bg-[#353535] p-[0.497vw]  flex items-center">
-        <div className=" w-fit  border-r-[#616161] border-r-[1px] mr-[1.069vw]">
-          <button className="mr-[1.069vw] px-[0.993vw] py-[0.473vw] rounded-[26.06px] bg-[var(--highlight-yellow)] text-black">
-            Video Editing
-          </button>
-        </div>
-        <div className="relative rounded-[35px] bg-[#484848] h-[1.088vw] w-[80%] mr-[1.069vw]">
-            <div className=" w-[4%] bg-[var(--highlight-yellow)] h-full rounded-[35px]"></div>
-        </div>
-        <button className="px-[0.993vw] py-[0.473vw] rounded-[26.06px] bg-[#272727] text-[#626262]">
-            Horray!
-          </button>
+    <div className="flex flex-col justify-between h-full">
+      <StepProgress/>
+      <div className="grow" >
+        {children}
       </div>
-      {children}
+      
     </div>
   );
 };
--- app/services/video-service/page.tsx
+++ app/services/video-service/page.tsx
@@ -1,19 +1,108 @@
 "use client";
-import React from "react";
+import CustomCheckBox from "@/app/_components/customCheckBox/CustomCheckBox";
+import React, { useEffect } from "react";
+import styles from "./videoService.module.css";
+import CustomCheckBoxText from "@/app/_components/customCheckBox/CustomCheckBoxText";
+import Link from "next/link";
 
 export default function page() {
+  useEffect(() => {
+    let items = document.getElementsByClassName("item") as any;
+    let next = document.getElementById("next");
+    let prev = document.getElementById("prev");
+
+    let active = 3;
+    function loadShow() {
+      let stt = 0;
+      items[active].style.transform = `none`;
+      items[active].style.zIndex = 1;
+      items[active].style.filter = "none";
+      items[active].style.opacity = 1;
+      for (var i = active + 1; i < items.length; i++) {
+        stt++;
+        items[i].style.transform = `translateX(${120 * stt}px) scale(${
+          1 - 0.2 * stt
+        }) perspective(16px) rotateY(-1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(5px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+      stt = 0;
+      for (var i = active - 1; i >= 0; i--) {
+        stt++;
+        items[i].style.transform = `translateX(${-120 * stt}px) scale(${
+          1 - 0.2 * stt
+        }) perspective(16px) rotateY(1deg)`;
+        items[i].style.zIndex = -stt;
+        items[i].style.filter = "blur(5px)";
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
+      }
+    }
+
+    console.log(items);
+
+    // loadShow();
+  }, []);
+
   return (
-    <div className=" bg-red-500 w-full">
-      <div className=" pt-[1.5vw] pl-[4.264vw] mb-[2.933vw]">
-        <h2 className=" mb-[1.041vw]"> What type of video would you like to create?</h2>
-        <h4>
-        Our talented video editors possess a wide range of skills that can create <hr className="border-0"/> different genres of videos depending on your needs!
-        </h4>
-      </div>
-      <div className=" flex justify-between items-center pl-[4.021vw] pr-[1vw]">
-          <div className=" flex flex-col gap-[1.041vw]">
-            
+    <div className=" flex flex-col justify-between h-full">
+      <div className={`${styles.editing} w-full `}>
+        <div className=" pt-[1.5vw] pl-[4.264vw] mb-[3.5vw]">
+          <h2 className=" mb-[1.041vw]">
+            {" "}
+            What type of video would you like to create?
+          </h2>
+          <h4>
+            Our talented video editors possess a wide range of skills that can
+            create <hr className="border-0" /> different genres of videos
+            depending on your needs!
+          </h4>
+        </div>
+        <div className=" flex items-center justify-between pl-[4.021vw] pr-[1vw]">
+          <div className={`${styles.btns} flex flex-col gap-[1.041vw]`}>
+            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Explainer</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Educational</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="checkbox">YouTube</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Social Media Ad</CustomCheckBoxText>
+            <CustomCheckBoxText btnSize="xl" inputType="checkbox">Short Film</CustomCheckBoxText>
           </div>
+          <div>
+            <div className={`${styles.slider} slider`}>
+              <div className={`${styles.item} item`}>
+                <h1>Slide 1</h1>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                magni magnam unde ipsam repudiandae explicabo expedita labore,
+                sequi minus neque beatae voluptatum, quasi accusamus quia quis
+                voluptas laborum ad! Ab totam doloribus, excepturi possimus rem
+                vel quia fugit molestiae officiis!
+              </div>
+              <div className={`${styles.item} item`}>
+                <h1>Slide 2</h1>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                magni magnam unde ipsam repudiandae explicabo expedita labore,
+                sequi minus neque beatae voluptatum, quasi accusamus quia quis
+                voluptas laborum ad! Ab totam doloribus, excepturi possimus rem
+                vel quia fugit molestiae officiis!
+              </div>
+              <div className={`${styles.item} item`}>
+                <h1>Slide 3</h1>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere
+                magni magnam unde ipsam repudiandae explicabo expedita labore,
+                sequi minus neque beatae voluptatum, quasi accusamus quia quis
+                voluptas laborum ad! Ab totam doloribus, excepturi possimus rem
+                vel quia fugit molestiae officiis!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-[100%] flex justify-between">
+        <button className=" bg-[#484848] px-[2vw] py-[0.5vw] font-semibold rounded-[41.03px] cursor-pointer">
+          Back
+        </button>
+        <Link href={"/services/video-service/footage-selection"}><button className=" bg-[#F8F24B] px-[2vw] py-[0.5vw] font-semibold rounded-[41.03px] text-[var(--primary-black)] cursor-pointer">
+          Next
+        </button></Link>
       </div>
     </div>
   );
