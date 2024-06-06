d049da394b63236d8739abe9503bc1b398579ad6
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-06-06 12:56:29 +0300

    edit pr route name

---
+++ app/services/pr-monetization/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+<>
+<div className=' h-full flex flex-col justify-between'>
+    <div>sad</div>
+    <div>sade</div>
+</div>
+</>
+)
+}
+
+export default page
