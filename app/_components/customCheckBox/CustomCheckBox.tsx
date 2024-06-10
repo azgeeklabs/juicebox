724ee67adeddd72f19e9b04a225cb4cee784b591
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-10 16:40:37 +0300

    commit

--- app/_components/customCheckBox/CustomCheckBox.tsx
+++ app/_components/customCheckBox/CustomCheckBox.tsx
@@ -15,8 +15,6 @@ function CustomCheckBox(props: IProps) {
       />
       <span>
         <svg
-          width="11"
-          height="9"
           viewBox="0 0 11 9"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
