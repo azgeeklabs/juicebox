6fcada023dfc21113c5bb42897250d8252bb6798
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-11 11:08:41 +0300

    commit

--- app/services/web-design/website-type/page.tsx
+++ app/services/web-design/website-type/page.tsx
@@ -9,24 +9,24 @@ function page() {
       nextLink="/services/web-design/domain-selection"
       backLink="/services/web-design"
     >
-      <div className="flex flex-col gap-16 justify-center items-center h-full">
+      <div className="flex flex-col gap-[var(--sy-64px)] justify-center items-center h-full">
         <div
           className={classNames(
-            "flex flex-col items-center gap-4 text-center",
+            "flex flex-col items-center text-center",
             styles.container
           )}
         >
-          <h2 className="text-3xl font-bold">
+          <h2 className="font-bold">
             What is the type of your website?
           </h2>
-          <p className="text-lg w-[60%]">
+          <p className="w-[60%]">
             Please select the option that best describes your project type. This
             helps us understand the specific requirements for your project.
           </p>
         </div>
-        <div className={classNames("flex gap-20", styles.cards)}>
-          <div className={classNames("flex flex-col gap-2", styles.card)}>
-            <div className="flex items-center justify-center relative">
+        <div className={classNames("flex", styles.cards)}>
+          <div className={classNames("flex flex-col gap-[var(--sy-8px)]", styles.card)}>
+            <div className={classNames("flex items-center justify-center relative", styles.healthcareCard)}>
               <img
                 src="/assets/website-type-healthcare.gif" alt="/assets/website-type-healthcare.gif"
                 className={classNames(styles.gifImg, "w-full")}
@@ -44,8 +44,8 @@ function page() {
               className="absolute opacity-0 inset-0 cursor-pointer"
             />
           </div>
-          <div className={classNames("flex flex-col gap-2", styles.card)}>
-            <div className="flex items-center justify-center relative">
+          <div className={classNames("flex flex-col gap-[var(--sy-8px)]", styles.card)}>
+            <div className={classNames("flex items-center justify-center relative", styles.shippingCard)}>
               <img
                 src="/assets/website-type-shipping.gif" alt="/assets/website-type-shipping.gif"
                 className={classNames(styles.gifImg, "w-full")}
@@ -63,8 +63,8 @@ function page() {
               className="absolute opacity-0 inset-0 cursor-pointer"
             />
           </div>
-          <div className={classNames("flex flex-col gap-2", styles.card)}>
-            <div className="flex items-center justify-center relative">
+          <div className={classNames("flex flex-col gap-[var(--sy-8px)]", styles.card)}>
+            <div className={classNames("flex items-center justify-center relative", styles.companyCard)}>
               <img
                 src="/assets/website-type-company.gif" alt="/assets/website-type-company.gif"
                 className={classNames(styles.gifImg, "w-full")}
@@ -82,8 +82,8 @@ function page() {
               className="absolute opacity-0 inset-0 cursor-pointer"
             />
           </div>
-          <div className={classNames("flex flex-col gap-2", styles.card)}>
-            <div className="flex items-center justify-center relative">
+          <div className={classNames("flex flex-col gap-[var(--sy-8px)]", styles.card)}>
+            <div className={classNames("flex items-center justify-center relative", styles.otherCard)}>
               <img
                 src="/assets/website-type-other.gif" alt="/assets/website-type-other.gif"
                 className={classNames(styles.gifImg, "w-full")}
