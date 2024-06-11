ca76b370c045f0d4aaeee1e81f4ae2f121cb891a
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-11 10:38:39 +0300

    commit

--- app/services/seo-link-building/advertising-details/page.tsx
+++ app/services/seo-link-building/advertising-details/page.tsx
@@ -43,7 +43,7 @@ const page = () => {
               />
 
               {/* Paste Link button */}
-              <button className="bg-[var(--highlight-yellow)] px-[1.892vw] py-[0.4vw] text-black rounded-[33px]">
+              <button className="bg-[var(--highlight-yellow)] px-[1.892vw] py-[0.4vw] text-black rounded-[var(--33px)]">
                 Paste Link
               </button>
             </div>
@@ -52,7 +52,7 @@ const page = () => {
             <h3 className="mb-[0.6vw]">Anchor Text</h3>
             <textarea
               rows={4}
-              className="outline-none w-full rounded-[12px] bg-[var(--dark-gray-3)] px-[1.088vw] py-[0.5vw] resize-none"
+              className="outline-none w-full rounded-[var(--12px)] bg-[var(--dark-gray-3)] px-[1.088vw] py-[0.5vw] resize-none"
               placeholder="Anchor Text"
             ></textarea>
             {/* Keywords */}
