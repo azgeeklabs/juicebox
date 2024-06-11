6fc7ef9b8a6c5df61b8bd2e21b83c44a22a9c60a
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-11 10:20:10 +0300

    commit

--- app/services/seo-campaign/advertising-details/page.tsx
+++ app/services/seo-campaign/advertising-details/page.tsx
@@ -8,31 +8,29 @@ const page = () => {
       backLink="/services/seo-campaign"
       nextLink="/services/seo-campaign/rank-selection"
     >
-      <div>
-        <div className={`${styles.advertisingDetails} w-full `}>
-          <div className=" text-center mx-auto mb-[3vw] mt-14">
-            <h2 className=" mb-[1.5vw]">
-              What product/service are you advertising?
-            </h2>
-            <h4>
-              Please provide the product link, name, and a brief description of
-              the product or service you are advertising.
-            </h4>
-          </div>
-          <div className="mx-auto w-fit">
-            <h3 className=" mb-[0.6vw]">
-              Product Link <span>(if exists)</span>
-            </h3>
-            <div className=" flex gap-[1vw] items-start mb-[1.5vw]">
-              <input
-                type="text"
-                placeholder="Product Link"
-                className="h-full mb-[1vw] w-[28.477vw] bg-[var(--dark-gray-3)] outline-none rounded-[71px] px-[1.088vw] py-[0.5vw] placeholder:text-[#FFFFFF80]"
-              />
-              <button className=" bg-[var(--highlight-yellow)] px-[1.892vw] py-[0.4vw] text-black rounded-[33px]">
-                Paste Link
-              </button>
-            </div>
+      <div className={`${styles.advertisingDetails} w-full `}>
+        <div className=" text-center mx-auto mb-[3vw]">
+          <h2 className=" mb-[1.5vw]">
+            What product/service are you advertising?
+          </h2>
+          <h4>
+            Please provide the product link, name, and a brief description of
+            the product or service you are advertising.
+          </h4>
+        </div>
+        <div className="mx-auto w-fit">
+          <h3 className=" mb-[0.6vw]">
+            Product Link <span>(if exists)</span>
+          </h3>
+          <div className=" flex gap-[1vw] items-start mb-[1.5vw]">
+            <input
+              type="text"
+              placeholder="Product Link"
+              className="h-full mb-[1vw] w-[28.477vw] bg-[var(--dark-gray-3)] outline-none rounded-[71px] px-[1.088vw] py-[0.5vw] placeholder:text-[#FFFFFF80]"
+            />
+            <button className=" bg-[var(--highlight-yellow)] px-[1.892vw] py-[0.4vw] text-black rounded-[var(--33px)]">
+              Paste Link
+            </button>
           </div>
         </div>
       </div>
