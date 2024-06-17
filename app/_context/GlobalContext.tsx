b84faa969446c5d132fed5ede8df12405ffda2e4
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-17 19:24:05 +0300

    initialContextState Object

--- app/_context/GlobalContext.tsx
+++ app/_context/GlobalContext.tsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from "react";
 
-// 1- create context, export it
-export const globalContext = createContext({
+const initialContextState = {
   test: false,
   setTest: (test: boolean) => {},
-});
+}
+
+// 1- create context, export it
+export const globalContext = createContext(initialContextState);
 
 // 2- provide context, export it
 export default function GlobalContextProvider({ children }: { children: React.ReactNode }) {
