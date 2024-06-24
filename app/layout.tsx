34333a24d033e1850ccda6dd6ba28974035e6a39
Abdulrahman Zaki <aazs.edu@gmail.com>
2024-06-25 00:55:42 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import "./globals.css";
 import type { Metadata } from "next";
 // import { Barlow } from "next/font/google";
-import MainWrapper from "./_components/layout/MainWrapper";
+
 // const barlow = Barlow({
 //   subsets: ["latin"],
 //   weight: ["100", "200", "300", "500", "600", "800", "900"],
 // });
 // Import Context (provider only)
 import GlobalContextProvider from "./_context/GlobalContext";
-// import Landing from "./landing/page";
+
 
 export const metadata: Metadata = {
   title: "Creative Juicebox",
