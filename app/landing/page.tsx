1e3b4b463867751f3de3fde8c98c55aa3d9f776f
azgeeklabs <abdulrahmanzakigeeklabs@gmail.com>
2024-06-25 01:24:59 +0300

    commit

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import React from "react";
 import Landing from "../_components/landing/Landing";
+import "./landingpage.css";
 
 export default function page() {
   return (
     <>
+      <div id="landing-overlay">
+        <div id="landing-overlay-content">
+          Sorry, this site is currently not available for mobile phones.
+        </div>
+      </div>
       <Landing />
     </>
   );
