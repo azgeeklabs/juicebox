312ae05a3765a7536b7003312d98a2bac2f7e8b2
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-06-24 17:34:30 +0300

    finish table in pr placement

--- app/dashboard/services/pr-placement/page.tsx
+++ app/dashboard/services/pr-placement/page.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import NextPrevNav from "@/app/_components/NextPrevNav/NextPrevNav";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+      <NextPrevNav nextLink="/dashboard/services/pr-placement/create-article">
+        <div className=" w-full">
+          <ul className=" flex w-full py-[--sy-22px] font-semibold">
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Site Name
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            DA/DR
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Type
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Niche
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Restrictions
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Cost
+            </li>
+            <li className=" w-[14.285%] flex justify-center items-center">
+            Get Started
+            </li>
+          </ul>
+          <div className=" w-full relative max-h-[60vh] overflow-y-scroll">
+            {Array(30)
+              .fill(0)
+              .map((_, i) => (
+                <ul className=" flex w-full py-[--sy-22px] border-b border-b-[#484848]">
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    PST Canada
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    <span className="px-[--16px] py-[--sy-8px] bg-[#484848] rounded-[--36px]">DA</span>
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    <span className="px-[--16px] py-[--sy-8px] bg-[#484848] rounded-[--36px]">200k </span>
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    <span className="px-[--16px] py-[--sy-8px] bg-[#484848] rounded-[--36px]">Politics</span>
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    <span className="px-[--16px] py-[--sy-8px] bg-[#484848] rounded-[--36px]">Follow</span>
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    230$
+                  </li>
+                  <li className=" w-[14.285%] flex justify-center items-center">
+                    <button className=" bg-[--highlight-yellow] rounded-[--36px] px-[--16px] py-[--sy-8px] font-semibold text-black">Get PR</button>
+                  </li>
+                </ul>
+              ))}
+          </div>
+        </div>
+      </NextPrevNav>
+    </>
+  );
+};
 
-export default page
+export default page;
