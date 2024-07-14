591b1c53ff844038a993bccba63707dd93f21d07
Adelgeeklabs <adelshokrygeeklabs@gmail.com>
2024-07-14 16:08:50 +0300

    finish sign up and login pages

---
+++ app/dashboard/profile/billing/page.tsx
@@ -0,0 +1,187 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./billing.module.css";
+
+const billing = () => {
+  return (
+    <div className=" w-full h-full">
+      <h1 className=" text-[--32px] font-semibold mb-[--sy-24px]">
+        Billing History
+      </h1>
+      <div className="bg-[#353535] rounded-[--7px] px-[--37px] py-[--sy-19px] mb-[--sy-8px]">
+        <h3 className=" font-bold mb-[--sy-18px]">Payment methods</h3>
+        <div className=" flex gap-[--28px] items-center">
+          <div className=" relative w-fit">
+            <svg
+              width="313"
+              height="167"
+              viewBox="0 0 313 167"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_3488_49919)">
+                <rect width="312.17" height="167" rx="18.5556" fill="#F8F24B" />
+                <rect
+                  x="203.297"
+                  y="63.1714"
+                  width="128.461"
+                  height="27.2876"
+                  rx="13.6438"
+                  transform="rotate(-30 203.297 63.1714)"
+                  fill="#272727"
+                  fill-opacity="0.17"
+                />
+                <rect
+                  x="174"
+                  y="80.0898"
+                  width="25.1046"
+                  height="27.2876"
+                  rx="12.5523"
+                  transform="rotate(-30 174 80.0898)"
+                  fill="#272727"
+                  fill-opacity="0.17"
+                />
+                <rect
+                  x="201.363"
+                  y="22.6992"
+                  width="231.399"
+                  height="27.2876"
+                  rx="13.6438"
+                  transform="rotate(-30 201.363 22.6992)"
+                  fill="#272727"
+                  fill-opacity="0.17"
+                />
+                <rect
+                  x="192.012"
+                  y="111.283"
+                  width="231.399"
+                  height="27.2876"
+                  rx="13.6438"
+                  transform="rotate(-30 192.012 111.283)"
+                  fill="#272727"
+                  fill-opacity="0.17"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_3488_49919">
+                  <rect width="312.17" height="167" rx="18.5" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+            <div className="text-black flex justify-between items-center w-full absolute top-[15%] px-[--23px]">
+              <span className=" font-medium">Card Number</span>
+              <svg
+                width="34"
+                height="11"
+                viewBox="0 0 34 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M30.8075 10.7575L30.5005 9.1751H27.0705L26.5247 10.7462L23.7755 10.7519C25.0843 7.5071 26.3959 4.26353 27.7104 1.02117C27.9341 0.471203 28.3312 0.191295 28.9165 0.194108C29.364 0.198328 30.0939 0.198327 31.1077 0.195514L33.2334 10.7533L30.8075 10.7575ZM27.8427 7.00756H30.053L29.2276 3.04102L27.8427 7.00756ZM10.1208 0.192701L12.885 0.195514L8.61179 10.7589L5.81345 10.7561C5.10977 7.96589 4.41485 5.17335 3.72868 2.37852C3.59224 1.82152 3.3221 1.43189 2.80227 1.24763C2.33974 1.08306 1.56887 0.82988 0.488281 0.485269V0.196921H4.90478C5.66883 0.196921 6.11498 0.578103 6.25824 1.36016C6.40286 2.14362 6.76579 4.13815 7.34974 7.34373L10.1208 0.192701ZM16.6835 0.195514L14.4977 10.7561L11.8672 10.7533L14.0502 0.192701L16.6835 0.195514ZM22.0182 0C22.8054 0 23.7973 0.253184 24.3676 0.485269L23.9065 2.67531C23.3907 2.46151 22.5421 2.17316 21.8285 2.18301C20.7916 2.20129 20.1503 2.64999 20.1503 3.0804C20.1503 3.78088 21.2637 4.13393 22.4098 4.89911C23.7182 5.77118 23.8901 6.55465 23.8737 7.40562C23.856 9.17228 22.4098 10.915 19.359 10.915C17.9673 10.8939 17.4653 10.773 16.3301 10.358L16.8103 8.07234C17.966 8.57168 18.4558 8.73062 19.4436 8.73062C20.3482 8.73062 21.1245 8.35366 21.1313 7.69679C21.1368 7.2298 20.8585 6.99772 19.8434 6.42102C18.8283 5.84292 17.4039 5.04258 17.423 3.43627C17.4461 1.37985 19.3358 0 22.0195 0H22.0182Z"
+                  fill="#1D1D1D"
+                />
+              </svg>
+            </div>
+            <div className="text-black flex justify-between items-center w-full absolute bottom-[15%] px-[--23px]">
+              <div className=" flex flex-col gap-[--sy-4px] font-medium">
+                <span className=" font-medium">John Doe Johnny</span>
+                <span className=" font-medium">1234 XXXX XXXX</span>
+              </div>
+             <span className=" font-medium">12/24</span>
+            </div>
+
+            <Link
+              href={"#"}
+              className=" underline text-sm right-0 bottom-full -translate-y-1/4 absolute"
+            >
+              Change Card
+            </Link>
+          </div>
+          <div className=" w-full">
+            <div className=" flex justify-between items-center w-full px-[--40px]">
+              <div className=" flex flex-col gap-[--sy-20px] w-1/3">
+                <h4 className=" font-semibold">Next Payment on</h4>
+                <span className="text-[--32px] font-semibold">07 Feb 2025</span>
+              </div>
+              <div className=" flex justify-center w-1/3">
+                <div className=" flex flex-col gap-[--sy-20px] w-1/3">
+                  <h4 className=" font-semibold">Monthly Payment</h4>
+                  <span className="text-[--32px] font-semibold">$200</span>
+                </div>
+              </div>
+              <div className=" flex justify-end w-1/3">
+                <div className=" flex flex-col gap-[--sy-20px] ">
+                  <h4 className=" font-semibold">Total Amount Paid</h4>
+                  <span className="text-[--32px] font-semibold">$600</span>
+                </div>
+              </div>
+            </div>
+            <div className=" w-full h-[1px] bg-[#b1b1b1] my-[--sy-25px]"></div>
+            <div className=" flex justify-between items-center w-full px-[--40px]">
+              <div className=" flex flex-col gap-[--sy-20px] w-1/3">
+                <h4 className=" font-semibold">Total Orders</h4>
+                <span className="text-[--32px] font-semibold">26</span>
+              </div>
+              <div className=" flex justify-center w-1/3">
+                <div className=" flex flex-col gap-[--sy-20px] w-1/3">
+                  <h4 className=" font-semibold">Total Ongoing Services</h4>
+                  <span className="text-[--32px] font-semibold">25</span>
+                </div>
+              </div>
+              <div className=" flex justify-end w-1/3">
+                <div className=" flex flex-col gap-[--sy-20px]">
+                  <h4 className=" font-semibold relative">
+                    Overdue payment{" "}
+                    <svg
+                      className="absolute left-full translate-x-full top-1/2 -translate-y-1/2"
+                      width="17"
+                      height="17"
+                      viewBox="0 0 17 17"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M7.5 12.5H9.16667V7.5H7.5V12.5ZM8.33333 5.83333C8.56944 5.83333 8.7675 5.75333 8.9275 5.59333C9.0875 5.43333 9.16722 5.23556 9.16667 5C9.16611 4.76444 9.08611 4.56667 8.92667 4.40667C8.76722 4.24667 8.56944 4.16667 8.33333 4.16667C8.09722 4.16667 7.89945 4.24667 7.74 4.40667C7.58056 4.56667 7.50056 4.76444 7.5 5C7.49944 5.23556 7.57945 5.43361 7.74 5.59417C7.90056 5.75472 8.09833 5.83444 8.33333 5.83333ZM8.33333 16.6667C7.18056 16.6667 6.09722 16.4478 5.08333 16.01C4.06945 15.5722 3.1875 14.9786 2.4375 14.2292C1.6875 13.4797 1.09389 12.5978 0.656668 11.5833C0.219446 10.5689 0.00055661 9.48556 1.05485e-06 8.33333C-0.000554501 7.18111 0.218334 6.09778 0.656668 5.08333C1.095 4.06889 1.68861 3.18694 2.4375 2.4375C3.18639 1.68806 4.06834 1.09444 5.08333 0.656667C6.09833 0.218889 7.18167 0 8.33333 0C9.485 0 10.5683 0.218889 11.5833 0.656667C12.5983 1.09444 13.4803 1.68806 14.2292 2.4375C14.9781 3.18694 15.5719 4.06889 16.0108 5.08333C16.4497 6.09778 16.6683 7.18111 16.6667 8.33333C16.665 9.48556 16.4461 10.5689 16.01 11.5833C15.5739 12.5978 14.9803 13.4797 14.2292 14.2292C13.4781 14.9786 12.5961 15.5725 11.5833 16.0108C10.5706 16.4492 9.48722 16.6678 8.33333 16.6667Z"
+                        fill="#F8F24B"
+                      />
+                    </svg>
+                  </h4>
+                  <span className="text-[--32px] font-semibold">1</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className=" w-full  ">
+        <ul className=" w-full flex justify-between items-center py-[--sy-6px] font-bold">
+          <li className=" w-[20%] text-center">Invoice</li>
+          <li className=" w-[20%] text-center">Billing Date</li>
+          <li className=" w-[20%] text-center">Service</li>
+          <li className=" w-[20%] text-center">Amount</li>
+          <li className=" w-[20%] text-center">PDF</li>
+        </ul>
+        <div className={`${styles.tableBody} overflow-y-scroll h-[37vh]`}>
+          {Array(6)
+            .fill(0)
+            .map((e) => (
+              <ul className=" w-full flex justify-between items-center py-[--sy-16px] border-b-[1px] border-[#3f3f3f]">
+                <li className=" w-[20%] text-center">Invoice 0001</li>
+                <li className=" w-[20%] text-center">07 January 2025</li>
+                <li className=" w-[20%] text-center">SEO</li>
+                <li className=" w-[20%] text-center">$200</li>
+                <li className=" w-[20%] text-center">
+                  <button className=" px-[--24px] py-[--sy-8px] rounded-[--26px] text-black bg-[#F8F24B] font-semibold">
+                    Download
+                  </button>
+                </li>
+              </ul>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default billing;
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,146 @@
+import { Divider } from "@mui/material";
+import Link from "next/link";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" bg-[#181818] h-full w-full flex justify-center items-center flex-col">
+      <div className=" px-[--38px] py-[--sy-33px] rounded-[--14px] bg-[#272727] mb-[--sy-30px]">
+        <h1 className=" font-semibold text-[--30px] mb-[--sy-20px]">
+          Get Started Now
+        </h1>
+        <p className=" mb-[--sy-18px] text-[#b1b1b1]">
+          Lorem ipsum dolor sit amet consectetur. Et amet suscipit quisque
+          consectetur <br /> maecenas amet elit. Vitae orci fringilla commodo
+          dignissim vulputate ac. Egestas.
+        </p>
+        <div className=" flex flex-col gap-[--sy-21px] mb-[--sy-40px]">
+          <div className=" flex gap-[--30px] items-center">
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="First Name">First Name</label>
+              <input
+                type="text"
+                id="First Name"
+                placeholder="John"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="Last Name">First Name</label>
+              <input
+                type="text"
+                id="Last Name"
+                placeholder="Doe"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+          </div>
+          <div className=" flex gap-[--30px] items-center">
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="Email">Email</label>
+              <input
+                type="email"
+                id="Email"
+                placeholder="John"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="Date of birth">Date of birth</label>
+              <input
+                type="text"
+                id="Date of birth"
+                placeholder="15/2/1996"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+          </div>
+          <div className=" flex gap-[--30px] items-center">
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="*******"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+            <div className=" flex flex-col gap-[--sy-16px] grow">
+              <label htmlFor="repassword">Re-enter Password</label>
+              <input
+                type="password"
+                id="repassword"
+                placeholder="*******"
+                className=" bg-[#484848] outline-none px-[--11px] py-[--sy-8px] rounded-[--3px]"
+              />
+            </div>
+          </div>
+        </div>
+        <button className=" block rounded-[--39px] text-black bg-[--highlight-yellow] w-full py-[--sy-19px] font-bold mb-[--sy-23px]">
+          Sign Up
+        </button>
+        <p className=" mb-[--sy-40px]">
+          By signing up you agree to our{" "}
+          <span className=" text-[--highlight-yellow]">Privacy Policy</span> and
+          our{" "}
+          <span className=" text-[--highlight-yellow]">Terms & Agreements</span>
+        </p>
+        <Divider
+          sx={{
+            "&.MuiDivider-root::before": {
+              backgroundColor: "#353535",
+            },
+            "&.MuiDivider-root::after": {
+              backgroundColor: "#353535",
+            },
+            marginBottom: "var(--sy-50px)",
+          }}
+        >
+          OR
+        </Divider>
+        <h2 className="text-[--20px] font-medium text-center mb-[--sy-30px]">
+          Register with:
+        </h2>
+        <div className=" flex items-center gap-[--16px]">
+          <button className=" flex items-center justify-center gap-[--15px] bg-[#353535] rounded-[--7px] grow py-[--sy-12px]">
+            <svg
+              width="19"
+              height="18"
+              viewBox="0 0 19 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18.0081 6.93033H9.68047C9.68047 7.79638 9.68046 9.52832 9.67516 10.3944H14.5008C14.3159 11.2604 13.6602 12.4731 12.7339 13.0837C12.7339 13.0837 12.7321 13.0888 12.7303 13.0879C11.4987 13.9011 9.87336 14.0857 8.66651 13.8432C6.77484 13.4673 5.27777 12.0954 4.66992 10.3529C4.67346 10.3503 4.67612 10.3263 4.67878 10.3246C4.29832 9.24372 4.29832 7.79638 4.67878 6.93033H4.67789C5.16806 5.33853 6.71024 3.88623 8.60456 3.48872C10.1282 3.16568 11.8473 3.51538 13.1117 4.6984C13.2798 4.53385 15.4386 2.42597 15.6006 2.25449C11.281 -1.65746 4.36467 -0.281347 1.77668 4.77033H1.77579C1.77579 4.77033 1.77668 4.77054 1.77138 4.78006C0.49109 7.2613 0.544176 10.185 1.78022 12.5476C1.77668 12.5502 1.77403 12.5518 1.77138 12.5544C2.89151 14.7282 4.93006 16.3953 7.38622 17.0301C9.99545 17.7143 13.316 17.2466 15.5404 15.2348L15.5431 15.2373C17.4277 13.5399 18.6009 10.6483 18.0081 6.93033Z"
+                fill="white"
+              />
+            </svg>
+            Google
+          </button>
+          <button className=" flex items-center justify-center gap-[--15px] bg-[#353535] rounded-[--7px] grow py-[--sy-12px]">
+            <svg
+              width="16"
+              height="18"
+              viewBox="0 0 16 18"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11.0595 2.76139C11.7202 2.02917 12.1655 1.00933 12.0437 -0.00537109C11.0916 0.0292906 9.93977 0.576206 9.2571 1.30756C8.64408 1.95659 8.10895 2.99364 8.25281 3.98842C9.31483 4.0638 10.3988 3.49446 11.0595 2.76139ZM13.441 9.20164C13.4676 11.8246 15.9518 12.6972 15.9793 12.7084C15.9591 12.7699 15.5825 13.9517 14.6708 15.1735C13.8818 16.2289 13.0635 17.2801 11.7742 17.3026C10.5079 17.3243 10.1001 16.6146 8.65141 16.6146C7.20362 16.6146 6.75096 17.28 5.5524 17.3242C4.30803 17.3667 3.35964 16.1823 2.56518 15.1303C0.939622 12.9787 -0.301997 9.05007 1.36571 6.39849C2.19407 5.08223 3.67393 4.24743 5.28117 4.22664C6.50263 4.20497 7.65629 4.97978 8.40309 4.97978C9.1499 4.97978 10.5519 4.04826 12.0253 4.18517C12.642 4.20857 14.3739 4.413 15.4854 5.90429C15.3956 5.95542 13.4191 7.00932 13.441 9.20164Z"
+                fill="white"
+              />
+            </svg>
+            Apple
+          </button>
+        </div>
+      </div>
+      <p>Already have an account? <Link href={"#"} className=" underline">Sign In</Link></p>
+    </div>
+  );
+};
+
+export default page;
