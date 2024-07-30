6aaf7c376eb312875b1688397ce987c31b17cb69
Abdulrahman Zaki <aazs.edu@gmail.com>
2024-07-31 00:00:55 +0300

    Reload the page when the window is resized

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,30 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 // import Home from "../_components/home/Home";
 import dynamic from "next/dynamic";
 
-const Home = dynamic(() => import('../_components/home/Home'), { ssr: false });
+const Home = dynamic(() => import("../_components/home/Home"), { ssr: false });
 
 export default function page() {
+  useEffect(() => {
+    // Reload the page when the window is resized
+    let resizeTimeout: NodeJS.Timeout; // Explicitly type the variable
+
+    const handleResize = () => {
+      clearTimeout(resizeTimeout);
+      resizeTimeout = setTimeout(() => {
+        window.location.reload();
+      }, 500); // Adjust the timeout as needed
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Home />
