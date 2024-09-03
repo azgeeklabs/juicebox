1029ea1dab099095bd29e69de6196bfc7115ae51
mohamedFathygeeklabs <mohamedfathygeeklabs@gmail.com>
2024-09-03 18:22:58 +0300

    feat: Update ProtectedRoute component to handle client-side rendering and authentication

--- app/_components/ProtectedRoute.tsx
+++ app/_components/ProtectedRoute.tsx
@@ -1,18 +1,40 @@
 "use client";
-import React from "react";
-// import { useAuth } from "../context/AuthContext";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "../_context/AuthContext";
+import image from "@/public/assets/bg-1.svg";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  //   const { user } = useAuth();
-  const { user } = { user: false };
+  const { user } = useAuth();
   const router = useRouter();
-  if (!user) {
-    router.push("/login");
-    return null;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // This ensures that the component renders only after the client has mounted
+
+    if (
+      typeof window !== "undefined" &&
+      !user.token &&
+      !localStorage.getItem("token")
+    ) {
+      router.push("/login");
+    }
+  }, [user, router]);
+
+  if (!isClient) {
+    // Return a placeholder while waiting for the client-side rendering
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <img
+          src={image.src}
+          className="animate-pulse animat w-[--313px]"
+          alt="logo"
+        />
+      </div>
+    );
   }
 
-  return children;
+  return <>{children}</>;
 };
 
 export default ProtectedRoute;
